d68e3338e4fdf7df35238d3371facc0a32dae7fc
Camelia03 <camelia_weber@yahoo.com>
2023-07-06 15:37:46 +0000

    Updating Categories

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -4,11 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
 
+db = SQLAlchemy()
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 from taskmanager import routes  # noqa
+
+with app.app_context():
+    db.create_all()
